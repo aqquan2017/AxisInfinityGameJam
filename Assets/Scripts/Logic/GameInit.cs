c1f08d3971dda264bc542c61dfc6ec1857ab77c9
MasterQQ <aqquan2017@gmail.com>
2022-08-11 02:50:01 +0700

    Load first axie

--- Assets/Scripts/Logic/GameInit.cs
+++ Assets/Scripts/Logic/GameInit.cs
@@ -1,11 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using AxieMixer.Unity;
+using Spine.Unity;
 using UnityEngine;
 
 public class GameInit : MonoBehaviour
 {
+    [SerializeField] private GameObject _axieTest;
+    
     void Start()
     {
+        TimerManager.Instance.Init();
+        UIManager.Instance.Init();
+        SceneController.Instance.Init();
+        SoundManager.Instance.Init();
+        
         Mixer.Init();
+        
     }
 }
