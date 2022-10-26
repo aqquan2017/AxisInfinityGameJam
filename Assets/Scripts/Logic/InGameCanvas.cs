910e0fda8ae43c72d222862cba02b34440923443
MasterQQ <aqquan2017@gmail.com>
2022-10-26 19:32:15 +0700

    Add exit btn in game

--- Assets/Scripts/Logic/InGameCanvas.cs
+++ Assets/Scripts/Logic/InGameCanvas.cs
@@ -9,11 +9,17 @@ public class InGameCanvas : MonoBehaviour
 {
     public Text _levelText;
     public Button _howToPlayBtn;
+    public Button _rewindBtn;
+    public Button _exitToMainMenuBtn;
+    public Button _resetGameBtn;
 
     void Start()
     {
         _levelText.text = "Level " + (SceneController.Instance.CurrentScene - 1).ToString();
         _howToPlayBtn.onClick.AddListener(OnHowToPlay);
+        _rewindBtn.onClick.AddListener(OnRewind);
+        _exitToMainMenuBtn.onClick.AddListener(OnExitToMainMenu);
+        _resetGameBtn.onClick.AddListener(OnResetLevel);
     }
 
     private void OnDestroy()
@@ -24,12 +30,27 @@ public class InGameCanvas : MonoBehaviour
     void OnHowToPlay()
     {
         UIManager.Instance.GetPanel<TextPopupPanel>().SetInfo("GUIDE" ,
-            "<color=#ff0000ff>W/A/S/D</color> or <color=#ff0000ff>ARROW KEY</color> to move axie around." +
-            "\n<color=#ff0000ff>SPACE</color> to reset level." +
-            "\n<color=#ff0000ff>ESCAPE</color> to go back the main menu." +
+            "<color=#ff0000ff>Swipe</color> to move Axie around." +
             "\n\n Please help poor Axie ^^"); 
             
         UIManager.Instance.ShowPanelWithDG(typeof(TextPopupPanel));
+        SoundManager.Instance.Play(Sounds.UI_POPUP);
+    }
+    void OnRewind()
+    {
+            
+        SoundManager.Instance.Play(Sounds.UI_POPUP);
+    }
+    
+    void OnResetLevel()
+    {
+        GameStatic.Instance.ResetGame();    
+        SoundManager.Instance.Play(Sounds.UI_POPUP);
+    }
+    
+    void OnExitToMainMenu()
+    {
+        UIManager.Instance.ShowPanelWithDG(typeof(ExitToMainMenuPanel));
         SoundManager.Instance.Play(Sounds.UI_POPUP);
     }
 }
